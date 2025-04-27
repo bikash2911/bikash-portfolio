84e0a21feb6fad68c52f8b4b5bcc3751e8aaca28
bikash2911 <raibikas2911@gmail.com>
2025-04-27 04:30:54 +0300

    fixed some ui/ux

--- script.js
+++ script.js
@@ -16,19 +16,4 @@ navLinks.forEach((link) => {
     nav_bar.classList.remove("active");
   });
 });
-// contact form ko lagi
 
-document.getElementById("form").addEventListener("submit", function (e) {
-  const name = document.getElementById("fullname").value.trim();
-  const phone = document.getElementById("phone").value.trim();
-
-  if (!/^[a-zA-Z\s]+$/.test(name)) {
-    alert("Full name should contain only letters and spaces.");
-    e.preventDefault();
-  }
-
-  if (!/^\d{7,15}$/.test(phone)) {
-    alert("Phone number should contain only digits (7–15 characters).");
-    e.preventDefault();
-  }
-});
